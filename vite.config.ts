7ad186875fabb850aaee250611c0f995c81a9c1f
Arun Eswaran <iaruneswar@gmail.com>
2026-08-08 11:45:18 +0530

    Update index.html and vite.config.ts base path to /Cinetix/

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: './',
+  base: '/Cinetix/',
   server: {
     host: true,
     port: 8080,
